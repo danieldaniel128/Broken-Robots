6d7919607af1eb8769a3980b93e9e0529dac08a9
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2023-10-23 23:08:01 +0300

    implementing ai state to state

--- Assets/Scripts/Data/ChaseState.cs
+++ Assets/Scripts/Data/ChaseState.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChaseState : MonoBehaviour
+public class ChaseState : AIState
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void EnterState(StateMachine stateMachine)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void ExitState(StateMachine stateMachine)
     {
-        
+
+    }
+
+    public override void UpdateState(StateMachine stateMachine)
+    {
+
     }
 }
