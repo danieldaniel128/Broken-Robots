ed98fc4c3b282c6df7e1e494151be68d1a8a3cf1
SummerNova <137454229+SummerNova@users.noreply.github.com>
2023-10-25 16:58:18 +0300

    New Cam

---
+++ Assets/Scripts/MovePivot.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePivot : MonoBehaviour
+{
+    
+    void Start()
+    {
+        transform.Translate(0, -2f, 0);
+    }
+
+  
+}
