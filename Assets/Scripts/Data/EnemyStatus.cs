b95823aa29cb299188aeac377b224eacec7b8314
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2023-11-01 19:12:50 +0200

    Enemy Status Script

---
+++ Assets/Scripts/Data/EnemyStatus.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStatus : MonoBehaviour
+{
+    public bool IsDead;
+    public bool IsAttacking;
+    public bool IsIdle;
+    public bool IsFlying;
+}
