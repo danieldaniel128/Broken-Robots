3bc2c730d1888acfeec373108e92d105067a59af
MatrixVVhite <yotamoscohen@gmail.com>
2023-11-02 16:24:51 +0200

    Added death condition to checkpoints

--- Assets/Scripts/WarpBackToCheckpoint.cs
+++ Assets/Scripts/WarpBackToCheckpoint.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class WarpBackToCheckpoint : MonoBehaviour
 {
     [SerializeField] private GameObject playerRef;
+    [SerializeField] private Health playerHP;
 
     public Transform lastCheckpoint;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision) //On contact with impassables
     {
         if (collision.gameObject.tag.Equals("Spikes"))
             playerRef.transform.position = lastCheckpoint.position;
     }
+
+    private void Update() //On death
+    {
+        if (playerHP.CurrentHealth <= 0)
+        {
+            playerRef.transform.position = lastCheckpoint.position;
+            playerHP.CurrentHealth = 9;
+        }
+    }
+
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag.Equals("Checkpoint"))
